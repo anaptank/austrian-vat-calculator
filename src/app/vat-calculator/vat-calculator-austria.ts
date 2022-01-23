a18250c0e79c97d47c4c3f843d2f2b57baf326b6
unknown <anapaulapianezer@hotmail.com>
2022-01-23 12:50:27 +0100

    feat: configured mathjs lib

--- src/app/vat-calculator/vat-calculator-austria.ts
+++ src/app/vat-calculator/vat-calculator-austria.ts
@@ -1,21 +1,22 @@
 import { CalculationResult, VatCalculator } from './vat-calculator';
+import { round } from 'mathjs';
 
 export class VatCalculatorAustria implements VatCalculator {
   CalculateFromNetValue(rate: number, netValue: number): CalculationResult {
-    const vatValue = netValue * rate;
-    const grossValue = netValue + vatValue;
+    const vatValue = round(netValue * rate);
+    const grossValue = round(netValue + vatValue);
     const result = new CalculationResult(netValue, vatValue, grossValue);
     return result;
   }
   CalculateFromVatValue(rate: number, vatValue: number): CalculationResult {
-    const netValue = vatValue / rate;
-    const grossValue = netValue + vatValue;
+    const netValue = round(vatValue / rate);
+    const grossValue = round(netValue + vatValue);
     const result = new CalculationResult(netValue, vatValue, grossValue);
     return result;
   }
   CalculateFromGrossValue(rate: number, grossValue: number): CalculationResult {
-    const netValue = grossValue / (1 + rate);
-    const vatValue = grossValue - netValue;
+    const netValue = round(grossValue / (1 + rate));
+    const vatValue = round(grossValue - netValue);
     const result = new CalculationResult(netValue, vatValue, grossValue);
     return result;
   }
