374ef87af10f29626f7c587a1fc83b884d00c9ec
unknown <anapaulapianezer@hotmail.com>
2022-01-23 22:39:02 +0100

    feat: layout changes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -15,6 +16,7 @@ const MaterialComponents = [
   MatIconModule,
   MatRadioModule,
   MatDialogModule,
+  MatToolbarModule,
 ];
 
 @NgModule({
